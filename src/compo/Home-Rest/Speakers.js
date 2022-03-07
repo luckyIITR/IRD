7120b26622460f26f2f41ec3e9a731f803807b68
Lucky Kushwaha <luckykushwaha56@gmail.com>
2022-03-07 11:58:45 +0530

    till 12pm

--- src/compo/Home-Rest/Speakers.js
+++ src/compo/Home-Rest/Speakers.js
@@ -1,23 +1,86 @@
 import React from "react";
-import speaker3 from "../images/speakers3.jpeg";
+import speaker1 from "../images/Speaker1.jpeg";
+import speaker2 from "../images/Speaker2.jpeg";
+import speaker3 from "../images/Speaker3.jpeg";
 
 function Speakers() {
   return (
-    <section className="container">
+    <div className="container">
       <h1 className="section-heading">Speakers</h1>
-      <div className="row">
-        <div className="speakers-col">
-          <img src={speaker3} alt="" />
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Molestias nesciunt fugiat ipsa architecto unde nihil nemo. Ea
-              animi nobis ut.
-            </p>
+      <div className="container-sp">
+        <div className="wrapper">
+          <div className="card">
+            <div className="card-thumb">
+              <img src={speaker1} className="client-img" alt="" />
+              <span className="client-name">
+                Professor Edward Witten
+                <br />
+                (IAS, Princeton)
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="time">
+                <i className="fas fa-clock"> </i>&nbsp; Mar 13, 6:30 pm
+              </div>
+
+              <h1>Black Hole Thermodynamics: Then and Now</h1>
+              <p className="review">
+                [in collaboration with the dept. of Physics; the lecture will
+                also be the third C. V. Raman Memorial Lecture],
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </section>
+      <div className="container-sp">
+        <div className="wrapper">
+          <div className="card">
+            <div className="card-thumb">
+              <img src={speaker2} className="client-img" alt="" />
+              <span className="client-name">
+                Prof. Sylvester James Gates Jr.
+                <br />
+                (Brown U.)
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="time">
+                <i className="fas fa-clock"> </i>&nbsp; Mar 13, 6:45 pm
+              </div>
+              <h2></h2>
+              <p className="review">
+                “People No One Can Imagine Anything Of & Following S.
+                Ramanujan’s Example” – Institute Lecture
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-sp">
+        <div className="wrapper">
+          <div className="card">
+            <div className="card-thumb">
+              <img src={speaker3} className="client-img" alt="" />
+              <span className="client-name">
+                Professor Abhay Ashtekar
+                <br />
+                (Penn State)
+              </span>
+            </div>
+            <div className="card-body">
+              <div className="time">
+                <i className="fas fa-clock"> </i>&nbsp; Mar 13, 8 pm
+              </div>
+              <h1></h1>
+              <p className="review">
+                People No One Can Imagine Anything Of & Following S. Ramanujan’s
+                Example” – Institute Lecture
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
