df751608814c290d9844bc380bc09721298c991f
luckyIITR <luckykushwaha56@gmail.com>
2022-03-10 23:53:58 +0530

    poster done

---
+++ src/compo/Home-Rest/Poster.js
@@ -0,0 +1,39 @@
+import React from "react";
+import poster from "../images/poster.jpeg";
+
+function readmehandler() {
+  var more = document.getElementById("more");
+
+  if (more.style.display === "none") {
+    more.style.display = "inline";
+  } else {
+    more.style.display = "none";
+  }
+}
+
+function Poster() {
+  return (
+    <>
+      <div className="container">
+        <button
+          className="ui button outline lifeAtIITRButton"
+          onClick={readmehandler}
+          id="myBtn"
+          style={{
+            margin: "70px auto 50px auto",
+            fontSize: "1em",
+            cursor: "pointer",
+          }}
+        >
+          View Poster
+        </button>
+        <span id="more">
+          <h1 style={{ fontSize: "3em", marginBottom: "50px" }}>Poster</h1>
+          <img src={poster} alt="poster" />
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default Poster;
--- src/compo/Home.js
+++ src/compo/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
 import Events from "./Home-Rest/Events";
+import Poster from "./Home-Rest/Poster";
 import Speakers from "./Home-Rest/Speakers";
 function Home() {
   return (
     <>
-      <Header />;
+      <Header />
+      <Poster />
       <Events />
       <Speakers />
     </>
